dc2fb5e72855446025b00420bb1a7a64ab59c5be
Chris Kansas <ckansas@smcm.edu>
2018-04-05 15:20:37 +0000

    Added in the signup route and the dashboard route which renders after signup.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(require('express-session')({
 }));
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,7 +27,28 @@ passport.deserializeUser(User.deserializeUser());
 
 //Routes
 app.get('/', function(req, res){
-   res.render('index'); 
+    res.render('index'); 
+});
+
+app.get('/dashboard', function(req, res) {
+    res.render('dashboard');
+});
+
+app.get('/signup', function(req, res){
+    res.render('signup');
+});
+
+app.post('/signup', function(req, res){
+    User.register(new User({email: req.body.email, username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.redirect('/signup');
+        } else {
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/dashboard'); 
+            });
+        }
+    }); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
