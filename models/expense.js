2ed20bce99ce473998011a771dc340298d49c9ff
Chris Kansas <ckansas@smcm.edu>
2018-04-10 15:22:43 +0000

    Created income and expense schemas/models.

---
+++ models/expense.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var expenseSchema = new mongoose.Schema({
+   amount: Number,
+   type: String, 
+   date: {
+       type: Date,
+       default: Date.now()
+   },
+   creator: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   }
+});
+
+module.exports = mongoose.model("Expense", expenseSchema);
